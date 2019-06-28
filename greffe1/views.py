d46c206690e889dfa5177ba126877ce3a12686c3
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-28 23:54:16 +0200

    Update views.py
    mais comme c pas ma page ben faudra lui dire faut que tu le fasses une deuxieme fois

--- greffe1/views.py
+++ greffe1/views.py
@@ -1,19 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .fonction import function
+from .fonction import ecrire
+from .fonction import lecture
+from .fonction import ecrire2
+
+
+
 
 def home(request):
     if request.method == "POST":
         page = request.POST.get('a')
 
-        print(page,'000000000000000000000,')
+        page = function(page)
+        ecrire(page)
 
 
+    
+        page_affichage = lecture()
+        ecrire2(page_affichage)
+        print(page_affichage)
 
-
-
-
-
+        return HttpResponse('ok')
 
 
 
     
     return render(request, 'home.html')
+
+
+
+
+def page(request):
+    return render(request, 'page.html')
