51ea8b6d258f31711869980d5c0a59f43dd07b3d
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-28 23:55:03 +0200

    Update fonction.py
    au pire on le film et on lui fait télécharger mais la faut que jdorme et c un peu moin cool que jlimaginé

--- greffe1/fonction.py
+++ greffe1/fonction.py
@@ -9,3 +9,24 @@ def function(path):
     var_soup = BeautifulSoup(page, "html.parser")
 
     
+    print(page)
+    return page
+
+PATH = r'C:\Users\jeanbaptiste\Desktop\greffe\env\greffe\greffe\requete.py'
+def ecrire(page):
+    
+    fichier = open(PATH, "w")
+    fichier.write(str(page))
+    fichier.close()
+
+def lecture():
+    fichier = open(PATH, "r")
+    fichier = fichier.read()
+    return str(fichier)
+
+PATH1 = r'C:\Users\jeanbaptiste\Desktop\greffe\env\greffe\templates\page.html'
+def ecrire2(page):
+    
+    fichier = open(PATH1, "w")
+    fichier.write(str(page))
+    fichier.close()
