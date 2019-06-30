2e468a19b0fcb3c48d414b2843f06b3378c7b82b
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-30 02:24:54 +0200

    Add files via upload

---
+++ greffe1/essais.py
@@ -0,0 +1,17 @@
+from fonction import function
+from fonction import ecrire
+from fonction import lecture
+from fonction import ecrire2
+
+
+page = 'https://fr.yahoo.com/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAMdlxFFv1CpIEQ0VuhLMZl4pjm_0Ur2KGpLoKBkg4lBqmzqdwLxulK-E29QEXf815EL1VsURfRYB-M3USUSs2fFR6tT63nGaOfQyk5mY4V9AltWx-EzQiluy32sS5KxDY0lQRsL6YmEXNMq4qWdOpBoyt2T6KtkfK9Bce2Dt8ViB'
+page = function(page)
+
+page = ecrire(page)
+
+
+
+page_affichage = lecture()
+ececrire2(page_affichage)
+
+
