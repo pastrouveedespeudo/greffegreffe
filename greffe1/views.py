6358a00ec6f93adffdd4eb94987e713a45a2a65c
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-06-30 02:24:18 +0200

    Update views.py

--- greffe1/views.py
+++ greffe1/views.py
@@ -11,18 +11,25 @@ from .fonction import ecrire2
 
 def home(request):
     if request.method == "POST":
+
+        #ici on récupere la page et l'étape
+        #par exemple 1ok donc inscription
+        #deux remplissage formulaire ect...
         page = request.POST.get('a')
-
-        page = function(page)
-        ecrire(page)
+        étape = request.POST.get('liste')
 
 
-    
-        page_affichage = lecture()
-        ecrire2(page_affichage)
-        print(page_affichage)
+        if page == "yahou":
+            page = 'https://fr.yahoo.com/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAMdlxFFv1CpIEQ0VuhLMZl4pjm_0Ur2KGpLoKBkg4lBqmzqdwLxulK-E29QEXf815EL1VsURfRYB-M3USUSs2fFR6tT63nGaOfQyk5mY4V9AltWx-EzQiluy32sS5KxDY0lQRsL6YmEXNMq4qWdOpBoyt2T6KtkfK9Bce2Dt8ViB'
+            page = function(page)
+            ecrire(page)
 
-        return HttpResponse('ok')
+
+        
+            page_affichage = lecture()
+            ecrire2(page_affichage)
+
+            return HttpResponse('ok')
 
 
 
