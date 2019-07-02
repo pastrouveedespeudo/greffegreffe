1bf82132105e51b9ffa770939b45c0b0cf2f41b4
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-03 01:21:43 +0200

    Update views.py

--- greffe1/views.py
+++ greffe1/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .fonction import function
-from .fonction import ecrire
-from .fonction import lecture
-from .fonction import ecrire2
+##from .fonction import function
+##from .fonction import ecrire
+##from .fonction import lecture
+##from .fonction import ecrire2
 
 
 
@@ -16,19 +16,15 @@ def home(request):
         #par exemple 1ok donc inscription
         #deux remplissage formulaire ect...
         page = request.POST.get('a')
-        étape = request.POST.get('liste')
+        étape = request.POST.get('b')
 
 
         if page == "yahou":
             page = 'https://fr.yahoo.com/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAMdlxFFv1CpIEQ0VuhLMZl4pjm_0Ur2KGpLoKBkg4lBqmzqdwLxulK-E29QEXf815EL1VsURfRYB-M3USUSs2fFR6tT63nGaOfQyk5mY4V9AltWx-EzQiluy32sS5KxDY0lQRsL6YmEXNMq4qWdOpBoyt2T6KtkfK9Bce2Dt8ViB'
-            page = function(page)
-            ecrire(page)
+            return HttpResponse('ok')
 
-
-        
-            page_affichage = lecture()
-            ecrire2(page_affichage)
-
+        if étape:
+            page = 'https://login.yahoo.com/?.src=ym&lang=&done=https%3A%2F%2Fmail.yahoo.com%2F%3F.intl%3Dfr%26.lang%3Dfr-FR%26.partner%3Dnone%26.src%3Dfp%26guce_referrer%3DaHR0cDovLzEyNy4wLjAuMTo4MDAwL3BhZ2U%26guce_referrer_sig%3DAQAAANaMkL1WAY3p59RCA2AOJ-PhiKoEDDaSoiKwrHneojN6D12eiM3Bw7wgsZQHW64-tp4a_Xqy4eOhYzHjilm0OeMFxdaGXHyJB0843EPfMLgGfJR-pdPfDeOfzlwaEYJvu3MbN2OeeVM7s7ZHLyXq8SQHtM8WDpKBsNSHSJztdNQd&.partner=none'
             return HttpResponse('ok')
 
 
@@ -40,4 +36,30 @@ def home(request):
 
 
 def page(request):
-    return render(request, 'page.html')
+    return render(request, 'inscription/page.html')
+
+def page1(request):
+    return render(request, 'inscription/page1.html')
+def page2(request):
+    return render(request, 'inscription/page2.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
